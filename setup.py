c91505f072f4fd49bb91c4e1a3ed1f2ea888da74
AntoineHo <antoine.houtain@hotmail.com>
2020-12-15 15:01:42 +0100

    reorganisation

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="happy-AntoineHo",
+    version="0.2.1",
+    author="Antoine Houtain",
+    author_email="antoine.houtain@gmail.com",
+    description="Haploidy with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AntoineHo/HapPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': ['happy=happy.hap:main',],
+    },
+)
